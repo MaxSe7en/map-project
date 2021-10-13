f4a22313fd54ae84798698e2b6918757b10de874
MaxSe7en <88913621+MaxSe7en@users.noreply.github.com>
2021-10-13 22:04:10 +0000

    Add files via upload

--- sample.js
+++ sample.js
@@ -3,20 +3,37 @@ const nest = document.querySelector(".nest");
 const container = document.getElementById("container");
 const signUp = document.getElementById("sign-up");
 const content = document.getElementById("content");
+const contents = document.querySelector(".content");
 const signUp2 = document.getElementById("footer");
 const close = document.querySelectorAll("#close");
 const signIn = document.getElementById("sign-in");
 const signIn2 = document.querySelector(".sign");
 const submit = document.querySelector(".submit");
 const submit2 = document.querySelector(".submit2");
-const mymap = document.getElementById("add-map");
 const form = document.getElementById("form");
 const form2 = document.getElementById("form2");
+
+// USER DETAILS GETTER
+const email1 = document.getElementById("email1");
+const passLog = document.getElementById("password");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+
+//GETTER FOR MAP ID
+const mymap = document.getElementById("add-map");
 
 console.log(close);
 close.forEach((element) => {
   element.addEventListener("click", () => {
     container.classList.remove("active");
+    email1.value = "";
+    passLog.value = "";
+    username.value = "";
+    email.value = "";
+    password1.value = "";
+    password2.value = "";
   });
 });
 
@@ -25,6 +42,7 @@ function logIn() {
   nest.classList.remove("active");
   container.classList.add("active");
   content.classList.add("active");
+  contents.classList.remove("active");
 }
 signIn.addEventListener("click", () => {
   logIn();
@@ -66,18 +84,15 @@ const users = [
 ];
 console.log(users);
 
+//FOR USER SIGN IN
 function login() {
-  const email1 = document.getElementById("email1");
-  const passLog = document.getElementById("password");
   const emailValue = email1.value.trim();
   const passwordValue = passLog.value.trim();
 
   //YOU CAN USE ANY OF THE FOLLOWING.
-
   const user = users.some((person) => {
     return person.passcode === passwordValue && person.email === emailValue;
   });
-  // console.log(user);
   if (user) {
     alert("login successful");
     glass.classList.remove("active");
@@ -85,11 +100,8 @@ function login() {
   } else alert("login unsuccessful");
 }
 
+//FOR USER SIGN UP
 function validate() {
-  const username = document.getElementById("username");
-  const email = document.getElementById("email");
-  const password1 = document.getElementById("password1");
-  const password2 = document.getElementById("password2");
   let usernameValue = username.value.trim();
   let emailValue = email.value.trim();
   let passwordValue = password1.value.trim();
@@ -103,7 +115,7 @@ function validate() {
     alert("Password cannot be blank");
   } else if (passwordValue !== password2Value) {
     alert("passwords do not match.");
-  } else if (passwordValue.length < 8) {
+  } else if (passwordValue.length < 6) {
     alert("password must be 8 or more");
   } else {
     alert("congrats!!! sign up successful");
@@ -128,16 +140,14 @@ function validate() {
       .then((data) => console.log(data));
 
     nest.classList.remove("active");
-
-    mymap.classList.add("active");
+    document.getElementById("get-username").innerText = usernameValue;
     username.value = "";
     email.value = "";
     password1.value = "";
     password2.value = "";
+    contents.classList.add("active");
   }
 }
-
-//If user does not log in, iwant to display the CONTENT again
 
 // BUILDING MY MAP
 
